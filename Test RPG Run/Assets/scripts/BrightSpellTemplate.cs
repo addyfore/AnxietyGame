ed9fbffa9767220775987c95551ba6c28a052ec9
Regentine <regentine@gmail.com>
2022-07-06 21:15:23 -1000

    Revert "please work"
    This reverts commit 32c3f451f1bd8ecf0d7a682fc487cca3b160f099.

--- Test RPG Run/Assets/scripts/BrightSpellTemplate.cs
+++ Test RPG Run/Assets/scripts/BrightSpellTemplate.cs
@@ -18,7 +18,12 @@ public class BrightSpellTemplate : SpellTemplate
     private GameObject hitObject;
     public LightPillars lightPillars;
 
+<<<<<<< Updated upstream
 
+
+        
+
+=======
     public override void Awake()
     {
         seasideRuinsEntrance.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 1f);
@@ -26,7 +31,7 @@ public class BrightSpellTemplate : SpellTemplate
         seasideRuinsEntrance.gameObject.GetComponent<CapsuleCollider2D>().enabled = true;
         base.Awake();
     }
-
+>>>>>>> Stashed changes
 
     public override void castSpell()
     {
